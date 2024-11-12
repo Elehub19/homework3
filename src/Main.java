893ef79301af25b2a6bd5363832d8de7d3c57005
Elehub19 <lopasheva@me.com>
2024-11-12 19:31:08 +0500

    hw3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        dog=dog+4;
+        System.out.println(dog);
+        cat=cat+4;
+        System.out.println(cat);
+        paper=paper+4;
+        System.out.println(paper);
+        dog=dog-3.5;
+        System.out.println(dog);
+        cat=cat-1.6;
+        System.out.println(cat);
+        paper=paper-7639;
+        System.out.println(paper);
+        var friend=19;
+        System.out.println(friend);
+        friend=friend*2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var m1=78.2;
+        System.out.println(m1);
+        var m2=82.7;
+        System.out.println(m2);
+        var m=m2+m1;
+        System.out.println(m);
+        var mr=m2-m1;
+        System.out.println(mr);
+        var mc=m2%m1;
+        System.out.println(mc);
+        var k=640/8;
+        System.out.println("'Всего работников в компании - "+k+" человек'");
+        k=k+94;
+        var h=k*8;
+        System.out.println("'Если в компании работает "+k+ " человека, то всего "+h+" часов работы может быть поделено между сотрудниками'");
     }
 }
+
+
